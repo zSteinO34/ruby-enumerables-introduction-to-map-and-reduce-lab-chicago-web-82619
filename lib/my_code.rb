4e3c7acf86c3ec3e8cdc3d0e901396ba61b3f7ad
Zac Steinberg <zstein1234@gmail.com>
2019-08-14 23:48:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,57 @@
-# My Code here....
+def map_to_negativize(source_array)
+  source_array.map { |index|
+    index * -1
+  }
+end
+
+def map_to_no_change(source_array)
+  source_array.map { |index|
+    index * 1
+  }
+end
+
+def map_to_double(source_array)
+  source_array.map { |index|
+    index * 2
+  }
+end
+
+def map_to_square(source_array)
+  source_array.map { |index|
+    index ** 2
+  }
+end
+
+def reduce_to_total(source_array, starting_point = 0)
+    source_array.reduce(starting_point) { |total, current_element| 
+      total + current_element
+    }
+end
+
+def reduce_to_all_true(source_array)
+  source_array.reduce(true, :&)
+end
+
+def reduce_to_any_true(source_array)
+  count = 0
+  while count < source_array.length do
+    current = source_array[count]
+    if current == true 
+      return true
+    end
+    count += 1
+  end
+  return false
+end
+
+
+
+
+
+
+
+
+
+
+
+
